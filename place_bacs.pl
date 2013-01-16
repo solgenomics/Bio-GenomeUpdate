7d737cf96b056926360abbb9f4e0e81a2a3e6e08
Jeremy D. Edwards <jde22@cornell.edu>
2013-01-16 15:11:27 -0500

    adjust scaffold agp coords with chromosome agp coords

--- place_bacs.pl
+++ place_bacs.pl
@@ -11,7 +11,8 @@ place_bacs.pl
 =head1 COMMAND-LINE OPTIONS
 
  -t  TPF file
- -a  AGP file
+ -s  scaffold AGP file
+ -c  chromosome AGP file
  -l  Tab-delimited file containing BAC names and start and end coordinates.
  -h  Help
 
@@ -26,57 +27,77 @@ use File::Slurp;
 use Getopt::Std;
 
 use Getopt::Std;
-our ($opt_t, $opt_l, $opt_a, $opt_h);
-getopts('t:a:l:h');
+our ($opt_t, $opt_l, $opt_s, $opt_c, $opt_h);
+getopts('t:s:c:l:h');
 if ($opt_h){
   help();
   exit;
 }
-if (!$opt_t || !$opt_l || !$opt_a) {
+if (!$opt_t || !$opt_l || !$opt_s || !$opt_c) {
     print "\nTPF, AGP and BAC name/coordinates filenames are required.\n\n\n";
     help();
 }
 
 my $tpf_input_file = $opt_t;
 my $input_tpf = read_file($tpf_input_file) or die "Could not open TPF input file: $tpf_input_file\n";
-my $agp_input_file = $opt_a;
-my $input_agp = read_file($agp_input_file) or die "Could not open AGP input file: $agp_input_file\n";
+my $scaffold_agp_input_file = $opt_s;
+my $scaffold_input_agp = read_file($scaffold_agp_input_file) or die "Could not open scaffold AGP input file: $scaffold_agp_input_file\n";
+my $chromosome_agp_input_file = $opt_c;
+my $chromosome_input_agp = read_file($chromosome_agp_input_file) or die "Could not open chromosome AGP input file: $chromosome_agp_input_file\n";
 my $bac_input_file = $opt_l;
 my $input_bacs = read_file($bac_input_file) or die "Could not open BAC name and coordinates file: $bac_input_file\n";
 my $tpf = Bio::GenomeUpdate::TPF->new();
-my $agp = Bio::GenomeUpdate::AGP->new();
+my $scaffold_agp = Bio::GenomeUpdate::AGP->new();
+my $chromosome_agp = Bio::GenomeUpdate::AGP->new();
 my $out_tpf;
 my @bac_lines;
 my @bacs;
 #my @sorted_bacs;
 my $ordered_tpf;
-my %agp_coords;
+my %chromosome_agp_offset;
+my %scaffold_agp_coords;
 
 $tpf->parse_tpf($input_tpf);
-$agp->parse_agp($input_agp);
+$scaffold_agp->parse_agp($scaffold_input_agp);
+$chromosome_agp->parse_agp($chromosome_input_agp);
 
 
 #create a TPF for output by copying the original and clearing the lines.  This preserves the other TPF info.
 #$out_tpf = $tpf;
 #$out_tpf->clear_tpf_lines();
 
-
-my %agp_lines;
-if ($agp->has_agp_lines()) {
-  %agp_lines = %{$agp->get_agp_lines()};
+my %scaffold_agp_lines;
+if ($scaffold_agp->has_agp_lines()) {
+  %scaffold_agp_lines = %{$scaffold_agp->get_agp_lines()};
 }
 
-foreach my $agp_line_key (keys %agp_lines){
-  my %coords;
-  if ($agp_lines{$agp_line_key}->get_line_type() eq 'sequence') {
-    $coords{'start'} = $agp_lines{$agp_line_key}->get_object_begin();
-    $coords{'end'} = $agp_lines{$agp_line_key}->get_object_end();
-    my $agp_accession = $agp_lines{$agp_line_key}->get_component_id();
-    $agp_accession =~ s/\.\d+//;
-    $agp_coords{$agp_accession} = \%coords;
+my %chromosome_agp_lines;
+if ($chromosome_agp->has_agp_lines()) {
+  %chromosome_agp_lines = %{$chromosome_agp->get_agp_lines()};
+}
+
+foreach my $agp_line_key (keys %chromosome_agp_lines){
+  if ($chromosome_agp_lines{$agp_line_key}->get_line_type() eq 'sequence') {
+    my $offset = $chromosome_agp_lines{$agp_line_key}->get_object_begin();
+    my $agp_accession = $chromosome_agp_lines{$agp_line_key}->get_component_id();
+    $chromosome_agp_offset{$agp_accession} = $offset;
   }
-
 }
+
+foreach my $agp_line_key (keys %scaffold_agp_lines){
+  my %coords;
+  if ($scaffold_agp_lines{$agp_line_key}->get_line_type() eq 'sequence') {
+    my $scaffold_name = $scaffold_agp_lines{$agp_line_key}->get_object_being_assembled();
+    my $offset = $chromosome_agp_offset{$scaffold_name};
+    $coords{'start'} = $scaffold_agp_lines{$agp_line_key}->get_object_begin() + $offset;
+    $coords{'end'} = $scaffold_agp_lines{$agp_line_key}->get_object_end() + $offset;
+    my $agp_accession = $scaffold_agp_lines{$agp_line_key}->get_component_id();
+    $agp_accession =~ s/\.\d+//;
+    $scaffold_agp_coords{$agp_accession} = \%coords;
+  }
+}
+
+
 
 @bac_lines = split (/\n/, $input_bacs);
 foreach my $line (@bac_lines) {
@@ -91,7 +112,7 @@ foreach my $line (@bac_lines) {
 
 #sort @bacs
 
-$ordered_tpf = $tpf->get_tpf_with_bacs_inserted(\@bacs, \%agp_coords);
+$ordered_tpf = $tpf->get_tpf_with_bacs_inserted(\@bacs, \%scaffold_agp_coords);
 my $out_str_from_tpf_ordered = $ordered_tpf->get_formatted_tpf();
 print $out_str_from_tpf_ordered."\n";
 
