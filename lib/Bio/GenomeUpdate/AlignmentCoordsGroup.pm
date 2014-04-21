00a248cad4134c3494c1df0930ff28ddf2773b3d
Surya Saha <ss2489@cornell.edu>
2014-04-21 16:42:13 -0400

    Added docs

--- lib/Bio/GenomeUpdate/AlignmentCoordsGroup.pm
+++ lib/Bio/GenomeUpdate/AlignmentCoordsGroup.pm
@@ -16,7 +16,7 @@ use Bio::GenomeUpdate::AlignmentCoords;
 
 =head1 DESCRIPTION
 
-    This package performs calculations on an array of objects containing alignment coordinates (e.g., from Nucmer show-coords output) for a single query sequence (e.g., from a BAC) relative to multiple reference sequences (e.g., chromosome pseudomolecules).
+    This package performs calculations on an array of objects containing alignment coordinates (e.g., from Nucmer/delta-filter/show-coords output) for a single query sequence (e.g., from a BAC) relative to multiple reference sequences (e.g., chromosome pseudomolecules).
 
 =cut
 
@@ -202,7 +202,7 @@ sub get_cluster_groups_sorted_by_length {
 
 =item C<get_id_coords_and_direction_of_longest_alignment_cluster_group ( $gap_percent, $gap_allowed )>
 
-Returns IDs, start and end coordiantes, total aligned sequence length, and direction for the longest proximity-grouped alignment clusters sorted by longest to shortest length of non-overlapping sequence covered by alignment clusters.  The proximity grouping is done using the specified length of an allowed gap between aligned clusters in the reference sequence ($gap allowed) plus an adjustment based on a percent of the length of the gap between the aligned clusters on the query sequence. 
+Returns IDs, start and end coordinates, total aligned sequence length, and direction for the longest proximity-grouped alignment clusters sorted by longest to shortest length of non-overlapping sequence covered by alignment clusters.  The proximity grouping is done using the specified length of an allowed gap between aligned clusters in the reference sequence ($gap allowed) plus an adjustment based on a percent of the length of the gap between the aligned clusters on the query sequence. 
 
 =cut
 
