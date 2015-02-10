41bb214260597c47e7dcc20453ee53b3ea06daf0
Surya Saha <ss2489@cornell.edu>
2015-02-10 18:15:15 -0500

    added debug msgs and fixed gap counting in

--- group_coords.pl
+++ group_coords.pl
@@ -241,7 +241,9 @@ sub calc_and_print_info {
     $total_to_end++;
   }
   if ($flagged==0) {
-    $total_extend += $start_gap_length + $end_gap_length;
+  	print STDERR "**",join(' ',$query_id, $ref_start, $ref_end, $query_start, $query_end, $sequence_aligned_in_clusters, $start_gap_length, 
+  		$end_gap_length, $internal_gap_length, $start_gap_length + $end_gap_length + $internal_gap_length),"\n\n";
+    $total_extend += $start_gap_length + $end_gap_length + $internal_gap_length;
     
     $total_ref_covered += $sequence_aligned_in_clusters;
     
@@ -275,7 +277,7 @@ print STDERR "Total reference extended by valid BAC hits:\t\t\t$total_extend\n";
 print STDERR "Total reference covered by valid BAC hits:\t\t\t$total_ref_covered\n";#includes gaps ($gap_size_allowed) between alignment clusters
 print STDERR "Total N's within reference covered by valid BAC hits:\t\t$total_ref_Ns_covered\n";#includes gaps ($gap_size_allowed) between alignment clusters
 print STDERR "Total novel sequence from valid BAC hits:\t\t\t",$total_ref_Ns_covered+$total_extend,"\n";#includes gaps ($gap_size_allowed) between alignment clusters
-print STDERR "Statistics from AGPs\n";
+print STDERR "\nStatistics from AGPs\n";
 print STDERR "Contig or component AGP (contigs and scaffold gaps)\n";
 print STDERR "Total gaps completely covered from contig AGP:\t\t\t$total_complete_contig_gaps_covered\n";
 print STDERR "Total length of gaps completely covered from contig AGP:\t\t\t$total_complete_contig_gap_length_covered\n";
