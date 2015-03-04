dbefb0ace41901b6b2d7f59b9772f3a5166486b7
Surya Saha <ss2489@cornell.edu>
2015-03-04 18:33:47 -0500

    writing out fasta of BAC end seqs

--- align_BACends_group_coords.pl
+++ align_BACends_group_coords.pl
@@ -6,14 +6,14 @@ group_coords.pl
 
 =head1 SYNOPSIS
 
-    align_BACends_group_coords.pl -g [gap size] -r [fasta] -q [fasta] -c [agp] -s [agp]
+    align_BACends_group_coords.pl -l [BAC end length] -r [fasta] -q [fasta] -c [agp] -s [agp]
 
 =head1 COMMAND-LINE OPTIONS
 
  -u  Sequence ID of chromosome with unmapped contigs 
+ -l  BAC end length (required)
  -r  Fasta file of reference (required)
  -q  Fasta file of query (assembled and singleton BACs, required)
- -g  Gap size allowed between aligned clusters in the reference sequence, typically the mean/median scaffold gap (required)
  -c  Contig or component AGP file for reference (includes scaffold gaps)
  -s  Chromosome AGP file for reference (with only scaffolds and gaps) 
  -t  Print header
@@ -30,15 +30,19 @@ use warnings;
 
 use Getopt::Std;
 use File::Slurp;
+use Scalar::Util qw(looks_like_number);
 use Bio::GenomeUpdate::AlignmentCoords;
 use Bio::GenomeUpdate::AlignmentCoordsGroup;
 use Bio::GenomeUpdate::AGP;
 use Bio::DB::Fasta;
+use Bio::SeqIO;
+use Bio::Seq;
 
-our ( $opt_r, $opt_q, $opt_u, $opt_t, $opt_h, $opt_c, $opt_s );
-getopts("r:q:u:c:s:t:h");
-if ( !$opt_c || !$opt_s || !$opt_r || !$opt_q ) {
-	print STDERR "Required parameters missing!! exiting..\n";
+
+our ( $opt_l, $opt_r, $opt_q, $opt_u, $opt_t, $opt_h, $opt_c, $opt_s );
+getopts("l:r:q:u:c:s:t:h");
+if ( !$opt_l || !$opt_c || !$opt_s || !$opt_r || !$opt_q ) {
+	print STDERR "\nRequired parameters missing!! exiting..\n\n";
 	help();
 }
 if ($opt_h) {
@@ -68,6 +72,7 @@ if ( defined $opt_s ) {
 
 my $unmapped_ID;
 my $print_header;
+my $BACend_length;
 if ($opt_u) {
 	$unmapped_ID = $opt_u;
 	print STDERR "Gg: $opt_u\n";
@@ -86,6 +91,13 @@ if ($opt_t) {
 		die("-t must be T or F\n");
 	}
 }
+if (looks_like_number($opt_l)){
+	$BACend_length = $opt_l;
+}
+else{
+	die("$opt_l is not a number");
+}
+  
 
 open( MIXED, ">mixed_qry_${opt_q}_ref_${opt_r}_group_coords.out" )
   or die
@@ -104,6 +116,55 @@ my $chr_input_agp      = read_file($chr_agp_input_file)
   or die "Could not open chr AGP input file: $chr_agp_input_file\n";
 my $chr_agp = Bio::GenomeUpdate::AGP->new();
 $chr_agp->parse_agp($chr_input_agp);
+
+###### create fasta file of BACends ###### 
+my $ref_db   = Bio::DB::Fasta->new( $opt_r, '-reindex' => 1 );
+my $query_db = Bio::DB::Fasta->new( $opt_q, '-reindex' => 1 );
+
+my $query_bacends_fasta = Bio::SeqIO->new( -file => "query_bacends.fasta" -format => 'Fasta');
+
+# Loop through sequence objects
+my $stream  = $query_db->get_PrimarySeq_stream();
+while (my $query_seq_obj = $stream->next_seq()) {
+	my ($bacend_source,$bacend_seq,$query_seq_length);
+	#5' end
+	$bacend_source = $query_seq_obj->display_id();
+	#$bacend_name .="_left_500";
+	$bacend_seq = $query_seq_obj->subseq(1,$BACend_length);
+	my $left_end_seq = Bio::Seq->new( -display_id => $bacend_source."_left_".$BACend_length
+										-seq => $bacend_seq);
+	$query_bacends_fasta->write_seq($left_end_seq);
+	
+	#3' end
+	$query_seq_length = length  $query_seq_obj->seq();
+	$bacend_seq = $query_seq_obj->subseq( $query_seq_length - $BACend_length + 1 , $BACend_length );
+	my $right_end_seq = Bio::Seq->new( -display_id => $bacend_source."_right_".$BACend_length
+										-seq => $bacend_seq);
+	$query_bacends_fasta->write_seq($right_end_seq);
+	
+	#TODO: remember the name, length of query
+}
+
+
+
+###### run mummer with optimized paramaters ######
+
+
+
+
+
+###### compute alignment and coverage statistics ######
+
+
+
+
+
+
+
+
+
+
+
 
 my $total                  = 0;
 my $total_smaller_than_20k = 0;    #for alignments covering < 20k on ref
@@ -124,8 +185,7 @@ my $total_complete_chr_gaps_covered          = 0;
 my $total_complete_chr_gap_length_covered    = 0;
 my $total_partial_chr_gaps_covered           = 0;
 my $total_partial_chr_gap_length_covered     = 0;
-my $ref_db   = Bio::DB::Fasta->new( $opt_r, '-reindex' => 1 );
-my $query_db = Bio::DB::Fasta->new( $opt_q, '-reindex' => 1 );
+
 
 
 print STDERR "Number of reference sequences: ";
@@ -149,18 +209,17 @@ sub help {
 
     Description:
 
-     This script groups aligned clusters and creates a tab delimited file with BAC alignment details.
+     This script creates a fasta of BAC ends, aligns to reference with mummer and groups aligned clusters to create a tab delimited file with BAC alignment details. Mixed and out of order alignments are written to separate files.
 
     Usage:
-      group_coords.pl -i [coords file] -g [gap size] -r [fasta] -q [fasta] -c [agp] -s [agp]
+      align_BACends_group_coords.pl -r [fasta] -q [fasta] -c [agp] -s [agp]
 
     Flags:
 
-    -i  COORDS file created by show-coords (required)
     -u  Sequence ID of chromosome with unmapped contigs 
+    -l  BAC end length (required)
     -r  Fasta file of reference (required)
     -q  Fasta file of query (assembled and singleton BACs)
-    -g  Gap size allowed between aligned clusters in the reference sequence, typically the mean/median scaffold gap (required)
     -c  Contig or component AGP file for reference (includes scaffold gaps)
     -s  Chromosome AGP file for reference (with only scaffolds and gaps) 
     -t  Print header
