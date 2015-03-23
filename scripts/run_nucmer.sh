12e9badee41e799f8d0c386d3f4ac7dce5d40808
Surya Saha <ss2489@cornell.edu>
2015-03-23 17:32:52 -0400

    added cmdline params

--- scripts/run_nucmer.sh
+++ scripts/run_nucmer.sh
@@ -4,11 +4,23 @@
 # PPath@Cornell/BTI
 # Purpose: Run nucmer pipeline to align BACs (param 1) to chr ref (param 2). NOT USING delta-filter -u 99 as diff BACs can contain same repeat and that does not make the alignment to ref chr invalid. 
 
-nucmer -l 100 -c 500 --noextend -p 500bp_qry_${1}__ref_${2} "$2" "$1"
-#delta-filter -l 500 -u 99 500bp_qry_${1}__ref_${2}.delta > 500bp_qry_${1}__ref_${2}.delta.filtered
-#delta-filter -l 500 -u 99 500bp_qry_${1}__ref_${2}.delta > 500bp_qry_${1}__ref_${2}.delta.filtered
-delta-filter -l 500 500bp_qry_${1}__ref_${2}.delta > 500bp_qry_${1}__ref_${2}.delta.filtered
-show-coords -c -d -l -q -T -o 500bp_qry_${1}__ref_${2}.delta.filtered > 500bp_qry_${1}__ref_${2}.delta.filtered.coords
+usage(){
+	echo "usage:
+	$0 <BAC fasta> <REF fasta>"
+	exit 1
+}
 
-#perl -I /lib group_coords.pl -i 500bp_qry_${1}__ref_${2}.delta.filtered.coords -g 100000 -u "dummy" -f "$2" -q "$1"  > 500bp.group_coords.out
+if [ "$#" -ne 2 ]
+then
+	usage
+fi
+
+printf "Query : %s \n" "$1"
+printf "Reference : %s \n" "$2"
+
+nucmer -l 100 -c 500 --noextend -p 500bp_qry_"${1}"__ref_"${2}" "$2" "$1"
+#delta-filter -l 500 -u 99 500bp_qry_"${1}"__ref_"${2}".delta > 500bp_qry_"${1}"__ref_"${2}".delta.filtered
+delta-filter -l 500 500bp_qry_"${1}"__ref_"${2}".delta > 500bp_qry_"${1}"__ref_"${2}".delta.filtered
+show-coords -c -d -l -q -T -o 500bp_qry_"${1}"__ref_"${2}".delta.filtered > 500bp_qry_"${1}"__ref_"${2}".delta.filtered.coords
+
 
