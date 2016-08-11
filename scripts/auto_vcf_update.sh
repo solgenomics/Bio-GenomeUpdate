ec6588b8da09f3bcce13f044edbcdcba0e8ac86d
bellerbrock <bje24@cornell.edu>
2016-08-11 21:24:50 +0000

    modified vcf update scripts to find and correct any indels on flipped scaffolds during update process

--- scripts/auto_vcf_update.sh
+++ scripts/auto_vcf_update.sh
@@ -1,31 +1,33 @@
-#!/bin/bash                                                                                                                                                                                                 
-#-------------------------------------------------------------------------------------------------------------------------------                                                                            
-# NAME   
+#!/bin/bash
+#-------------------------------------------------------------------------------------------------------------------------------
+# NAME
 #
-# auto_vcf_update.sh 
-# 
+# auto_vcf_update.sh
+#
 # SYNOPSIS
-# Shell script for updating VCF file coordinates. AGP files must be given as command line arguments.
+# Shell script for updating VCF file coordinates. AGP files and new ref seq must be given as command line arguments.
 # Script will automatically update all vcf files in the current directory unless glob at step 3 is modified.
-# Step 5 and 6 can also be modified to change speed / CPU core usage. This script should be run from the 
+# Step 5 and 6 can also be modified to change speed / CPU core usage. This script should be run from the
 # scripts directory
 #
-# ./auto_update_vcf_coordinates.sh -o [old AGP file] -n [new AGP file]
+# ./auto_update_vcf_coordinates.sh -o [old AGP file] -n [new AGP file] -f [new ref seq FASTA file]
 #
-# To run, this script requires create_pseudo_gff_from_vcf.pl
+# To run, this script requires:
+#                      samtools
+#                      create_pseudo_gff_from_vcf.pl
 #                      split_by_lines.sh
 #                      update_coordinates_gff.pl
 #                      copy_updated_coordinates_to_vcf.pl
 #-------------------------------------------------------------------------------------------------------------------------------
-                                                                                                                                                          
+
 
 function split_by_chromosome {
-    # count number of unique chromosomes in file                                                                                                                                                          
-       
+    # count number of unique chromosomes in file
+
     CHROMS=($(cut -f1 < $1 | uniq | grep -v '^#'))
 
     LABEL=($(echo ${CHROMS[0]} | sed -r 's/([0-9]+)$//'))
-    
+
     NUMBERS=($(for chrom in ${CHROMS[*]}; do echo $chrom; done | sed -r 's/^(.*)([0-9][0-9])$/\2/' | grep -v '00'))
 
     for num in ${NUMBERS[*]}; do
@@ -85,23 +87,29 @@ do
 key="$1"
 
 case $key in
-    -o|--old.agp)
-    OLD_AGP="$2"
-    shift
-    ;;
-    -n|--new.agp)
-    NEW_AGP="$2"
-    ;;
+  -f|--fasta)
+  FASTA="$2"
+  shift
+  ;;
+  -o|--old.agp)
+  OLD_AGP="$2"
+  shift
+  ;;
+  -n|--new.agp)
+  NEW_AGP="$2"
+  ;;
 esac
 shift
 done
+echo FASTA  = "$FASTA"
 echo OLD_AGP  = "$OLD_AGP"
 echo NEW_AGP  = "$NEW_AGP"
-if [ -z "$OLD_AGP" ] || [ -z "$NEW_AGP" ]
+if [ -z "$FASTA" ] || [ -z "$OLD_AGP" ] || [ -z "$NEW_AGP" ]
 then
     echo "Trouble reading command line arguments, make sure
+    -f [new ref seq FASTA] and
     -o [old AGP] and
-    -n [new AGP] are both specified";
+    -n [new AGP] are all specified";
     exit
 fi
 
@@ -110,31 +118,31 @@ fi
 #----------------------------------------------------------------------------------
 
 echo Splitting agp files...
- 
+
 split_by_chromosome ${OLD_AGP}
 OLD_CHROM_LABEL=$LABEL
 echo OLD_CHROM_LABEL = "$OLD_CHROM_LABEL"
 
-split_by_chromosome ${NEW_AGP} 
+split_by_chromosome ${NEW_AGP}
 NEW_CHROM_LABEL=$LABEL
 echo NEW_CHROM_LABEL = "$NEW_CHROM_LABEL"
 
 #--------------------------------------------------------------------------------
-# 3 create pseudo gff files from each vcf file in parallel                                                                                                                                             
+# 3 create pseudo gff files from each vcf file in parallel
 #-------------------------------------------------------------------------------
 
 ls *.vcf | parallel ./create_pseudo_gff_from_vcf.pl
 
 
 #--------------------------------------------------------------------------------------------------
-# 4 make directories for temporary and final output                                                                                                                                                         
-# 5 split pseudo gff files by chromosome and line,   *split_by_lines script can be modified 
-#                                                     to determine line cutoff. Default is 2000 
+# 4 make directories for temporary and final output
+# 5 split pseudo gff files by chromosome and line,   *split_by_lines script can be modified
+#                                                     to determine line cutoff. Default is 2000
 #
 # 6 then update their coordinates in parallel        * parallel command is restricted to use 12
-#                                                      CPUs as default. Feel free to change.                                                                                                        
+#                                                      CPUs as default. Feel free to change.
 #--------------------------------------------------------------------------------------------------
- 
+
 mkdir updated_vcfs
 
 echo Output directory updated_vcfs created
@@ -142,12 +150,12 @@ echo Starting parallelized coordinate update tasks
 
 for file in *pseudo.gff* ; do
 
-mkdir temp_update_files 
+mkdir temp_update_files
 
 ./split_by_lines.sh $file | parallel -j 12 --verbose --rpl '{..} s/^.*([0-9][0-9])\.[0-9]+$/\1/;' "../update_coordinates_gff.pl -o ${OLD_AGP}.${OLD_CHROM_LABEL}{..} -n ${NEW_AGP}.${NEW_CHROM_LABEL}{..} -g {} -m {}_mapped -c 0" ;
 
 #------------------------------------------------------------------------------------------------
-# 7 compile  all split chromosome files for current accession back into a single updated gff file 
+# 7 compile  all split chromosome files for current accession back into a single updated gff file
 #------------------------------------------------------------------------------------------------
 
 combine_by_chromosome $file ;
@@ -155,18 +163,19 @@ combine_by_chromosome $file ;
 #-----------------------------------------------------------------------------------------------
 # 8 take updated coordinates and info from original vcf file to create updated vcf file
 #-----------------------------------------------------------------------------------------------
- 
-./copy_updated_coordinates_to_vcf.pl temp_update_files/$file.$NEW_CHROM_LABEL $file $NEW_CHROM_LABEL; 
+
+samtools faidx $FASTA;
+./copy_updated_coordinates_to_vcf.pl temp_update_files/$file.$NEW_CHROM_LABEL $file $FASTA $NEW_CHROM_LABEL;
 
 #-----------------------------------------------------------------------------------------------
-# 9 remove files temporarily created during update process                                                                                                                                              
-#------------------------------------------------------------------------------------------------ 
+# 9 remove files temporarily created during update process
+#------------------------------------------------------------------------------------------------
 
 rm -r temp_update_files
 rm $file
 
 done
-                                                                          
+
 #--------------------------------------------------------------------------------------------------
 # 10 report successful update
 #--------------------------------------------------------------------------------------------------
@@ -178,7 +187,5 @@ function finish {
     rm *.agp.$NEW_CHROM_LABEL*
 }
 trap finish EXIT
-                                                                                                                                                                                  
+
 echo Finished. Updated vcf files stored in updated_vcfs directory
-
-
--- scripts/copy_updated_coordinates_to_vcf.pl
+++ scripts/copy_updated_coordinates_to_vcf.pl
@@ -3,7 +3,7 @@
 =head1 NAME
 copy_updated_coordinates_to_vcf.pl
 =head1 SYNOPSIS
-copy_updated_coordinates_to_vcf.pl [updated pseudo GFF file] [old VCF file]
+copy_updated_coordinates_to_vcf.pl [updated pseudo GFF file] [old VCF file] [FASTA file] [chrom_label]
 =cut
 
 use warnings;
@@ -11,7 +11,8 @@ use strict;
 
 my $GFF = $ARGV[0];
 my $VCF = $ARGV[1];
-my $chrom_label = $ARGV[2];
+my $FASTA = $ARGV[2];
+my $chrom_label = $ARGV[3];
 
 my $build_name = $chrom_label;
 $build_name =~ s/(.*)ch$/$1/;
@@ -21,49 +22,68 @@ $VCF =~ s/.pseudo.gff(.*)/.vcf$1/;
 my $new_vcf = $VCF;
 $new_vcf =~ s/(.*)/updated_vcfs\/$1/;
 
-open(MAPPEDGFF, "<", $GFF) || die "Can't open gff file $GFF"; 
+open(MAPPEDGFF, "<", $GFF) || die "Can't open gff file $GFF";
 
-open(OLDVCF, "<", $VCF) || die "Can't open vcf file $VCF"; 
+open(OLDVCF, "<", $VCF) || die "Can't open vcf file $VCF";
 
 open(my $NEWVCF, ">", $new_vcf) || die "Can't create new VCF file $new_vcf";
-
-# for troubleshooting
-#open(my $FLIPLOG, '>', 'flip_log.txt') || die "Can't create flip_log.txt: $!";
 
 my $data_line_counter = 0;
 
 for (<OLDVCF>) {
     chomp;
     if (m/^##/) {
-	print $NEWVCF $_ . "\n";
+	    print $NEWVCF $_ . "\n";
     }
     elsif (m/^#/) {
-	print $NEWVCF "##mapped to $build_name " . localtime . "\n";
-	print $NEWVCF $_ . "\n";
+	    print $NEWVCF "##mapped to $build_name " . localtime . "\n";
+	    print $NEWVCF $_ . "\n";
     }
     else {
-	my ($chrom, $position, $id, $ref, $alt, @extra) = split /\t/;
-	chomp (my $gff_string = <MAPPEDGFF>);
-       	my @values = split /\t/, $gff_string;
-	my $new_position = $values[3];
-	my $new_orientation = $values[6];
-	$chrom =~ s/^(.*)([0-9][0-9])$/$chrom_label$2/;
-	
-	# before printing, correct ref and alt alleles if scaffold has been flipped in new genome build
-	if ($new_orientation eq '-') {
-	    #print $FLIPLOG join "\t", ($chrom, $new_orientation, $new_position);
-	    #print $FLIPLOG "\n";
-	    my $new_ref = &replace_with_complementary_base($ref);
-	    my $new_alt = &replace_with_complementary_base($alt);
-	    print $NEWVCF join "\t", ($chrom, $new_position, $id, $new_ref, $new_alt, @extra);
-	    print $NEWVCF "\n";
-	}
-	else {
-	    print $NEWVCF join "\t", ($chrom, $new_position, $id, $ref, $alt, @extra); 
-	    print $NEWVCF "\n";
-	}
+	    my ($chrom, $position, $id, $ref, $alt, $qual, $filter, $info, @extra) = split /\t/;
+      chomp (my $gff_string = <MAPPEDGFF>);
+      my @values = split "\t", $gff_string;
+      my $new_position = $values[3];
+      my $new_orientation = $values[6];
+      $chrom =~ s/^(.*)([0-9][0-9])$/$chrom_label$2/;
+
+	    if ($new_orientation eq '-') { #if scaffold has been flipped
+        my ($new_ref, $new_alt);
+
+        if ($info =~ m/^INDEL/) { #if indel, fix position, get new leading ref base, and calculate new ref and alt seqs (reverse complements of old seqs)
+
+          my $indel_length = length $ref;
+          $new_position = $new_position - $indel_length;
+          my $new_ref_base = &get_ref_base_by_position($FASTA, $chrom, $new_position);
+
+          $new_ref = &find_reverse_compliment($ref, $new_ref_base);
+
+          my @old_seqs = split ",", $alt;
+          my @new_seqs;
+          foreach my $seq (@old_seqs) {
+            push @new_seqs, &find_reverse_compliment($seq, $new_ref_base);
+          }
+          $new_alt = join ",", @new_seqs;
+
+          print STDERR "ref $ref with alt $alt is an INDEL\n";
+          print STDERR "new leading base at position $new_position not affected by indel event is $new_ref_base\n";
+          print STDERR "corrected to ref $new_ref and alt $new_alt\n";
+
+	      } else { #if a SNP, find simple complement
+          $new_ref = &replace_with_complementary_base($ref);
+          $new_alt = &replace_with_complementary_base($alt);
+        }
+
+        print $NEWVCF join "\t", ($chrom, $new_position, $id, $new_ref, $new_alt, $qual, $filter, $info, @extra);
+        print $NEWVCF "\n";
+
+      } else { # if not flipped
+
+	       print $NEWVCF join "\t", ($chrom, $new_position, $id, $ref, $alt, $qual, $filter, $info, @extra);
+         print $NEWVCF "\n";
+      }
     }
-}
+  }
 
 # sort new vcf file to ensure newly mapped coordinates are in order
 
@@ -72,7 +92,7 @@ system ("grep -v '^#' $new_vcf | sort -V >> $new_vcf.sorted");
 system ("mv $new_vcf.sorted $new_vcf");
 
 print "New vcf file $new_vcf with updated coordinates successfully created.\n";
-   
+
 sub replace_with_complementary_base {
     if ($_[0] eq 'A') {
 	return 'T';
@@ -88,17 +108,27 @@ sub replace_with_complementary_base {
     }
 }
 
+sub find_reverse_compliment {
+  my $old_seq = $_[0];
+  my $new_ref_base = $_[1];
+  my (@new_bases, $new_seq);
 
+  foreach my $base (split '', $old_seq) {
+    push @new_bases, &replace_with_complementary_base($base);
+  }
+  @new_bases = reverse @new_bases;
+  my $old_leading_ref_base = pop @new_bases;
+  unshift @new_bases, $new_ref_base;
+  $new_seq = join "", @new_bases;
+  return $new_seq;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+sub get_ref_base_by_position {
+  my $fasta = $_[0];
+  my $chrom = $_[1];
+  my $pos = $_[2];
+  my @results = `samtools faidx $fasta $chrom:$pos-$pos`;
+  my $ref_base = $results[1];
+  chomp $ref_base;
+  return $ref_base;
+}
