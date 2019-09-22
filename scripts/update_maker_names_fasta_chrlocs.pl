d94d9248d730b7bff17e2768e348449426981731
Surya Saha <ss2489@cornell.edu>
2019-09-22 19:22:17 -0400

    mRNA name from ID attr

--- scripts/update_maker_names_fasta_chrlocs.pl
+++ scripts/update_maker_names_fasta_chrlocs.pl
@@ -68,14 +68,14 @@ foreach my $line (@gff_lines) {
 	my @gff_line_arr = split ( "\t", $line);
 	if ($gff_line_arr[2] eq 'mRNA'){
 		my $chr = $gff_line_arr[0];
-		$chr =~ s/\$chrprefix//; #remove genome version and sc, so only sc number
+		$chr =~ s/$chrprefix//; #remove genome version and sc, so only sc number
 		my @gff_line_attr_arr = split (/;/, $gff_line_arr[8]);
-		my $name;
+		my $ID;
 		foreach my $attr (@gff_line_attr_arr){
-				if ( $attr =~ m/^Name\=/ ){
-					$name = $attr;
-					$name =~ s/^Name\=//;
-					$chr_location{$name} = $chr;
+				if ( $attr =~ m/^ID\=/ ){
+					$ID = $attr;
+					$ID =~ s/^ID\=//;
+					$chr_location{$ID} = $chr;
 					last;
 				}
 		}
@@ -89,29 +89,34 @@ foreach my $line (@lines) {
 	chomp($line);
 	my $new_id;
 
-	if ( $line =~ m/^>/ ) {
+	if ( $line =~ m/^>/ ) { #e.g. maker_curated_gene_coding_models.gff
 
-		print $line."\n";
+		print STDERR "\n".$line."\n";
+		
 		$line =~ s/^>//;
 		$new_id_index_output = $new_id_index_output.$line;
+		my $mRNA_chr_location = $chr_location{$line};
+
+		print STDERR "loc: $mRNA_chr_location\n";
 
 		my $current_maker_id = $line;
-		$current_maker_id =~ s/[0-9]+$//;
+		$current_maker_id =~ s/[0-9]+$//;#remove number suffix
 
 		if(($last_maker_id ne $current_maker_id) && ($seq_counter > 0)){
 			$last_id += 10; #namespace for 9 new genes now
 		}
 
-		$line =~ s/^[\S]+-mRNA-//;
+		$line =~ s/^[\S]+-mRNA-//;#remove suffix
 		my $mRNA_rank = $line;
-		#print "$mRNA_rank\n";
+		
+		print STDERR "rank: $mRNA_rank\n";
 
 		#presuming a gene space of <1,00,000 so 6 characters, e.g. 00001 - 99999, was 1 mill earlier
 		my @chars = split //,$last_id;
 		my $padding_count = 5 - scalar @chars;
 
-		#prefix with chr number
-		$new_id = $prefix.$chr_location{$line};
+		#prefix with chr number and 0's
+		$new_id = $prefix.$mRNA_chr_location;
 		foreach (1..$padding_count){
 			$new_id = $new_id.'0';
 		}
@@ -171,6 +176,7 @@ sub help {
         -i  Maker Fasta file (required)
         -p  Prefix for name, e.g DcitrP (required)
         -s  Starting seed, e.g. 1 (required)
+				-c  Prefix for chromosome in GFF e.g. Dc3.0sc (required)
         -g  GFF file with chromosomal location (required)
         -h  Help
 
