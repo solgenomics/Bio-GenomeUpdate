89e3ee766f83c4b7600e41e850d15f131ee9d748
Surya Saha <ss2489@cornell.edu>
2019-10-21 10:40:26 -0400

    gene handling

--- scripts/add_OGSids_maker-apollo_gff.pl
+++ scripts/add_OGSids_maker-apollo_gff.pl
@@ -15,7 +15,7 @@ add_OGSids_maker-apollo_GFF.pl -g [old Merged Maker and Apollo GFF file] -a [AHR
  -f  Curated descriptions from Apollo, e.g. 18712462-6e8c-478a-83c8-a40c9d0be977  Dihydrolipoyl transacetylase (required)
  -p  Prefix for name, e.g DcitrP (required)
  -c  Prefix for chromosome in GFF e.g. Dc3.0sc (required)
- -s  Starting seed, e.g. 1 (required)
+ -s  Starting seed for gene number for each chromosome, e.g. 1000 (required)
  -o  output GFF file with OGS ids
  -h  Help
 
@@ -78,12 +78,12 @@ foreach my $line (@lines) {
 # output variables
 my ($gff_output, $index_output, $desc_output);
 # tracking variables
-my ($last_id);
+my (%scaffold_last_gene_id, %gene_mRNA_last_rank, %gene_old_new_index, %mRNA_old_new_index);
 
 $gff_output   = '';                                                 # initialize
 $index_output = '';
 $desc_output  = '';
-$last_id      = 990;
+
 
 
 @lines = split( /\n/, $gff_input );
@@ -91,18 +91,59 @@ foreach my $line (@lines){
 	# comments line
 	if ( $line=~ m/^#/ ){
 		$gff_output .= $line."\n";
+
+		#add seqid to hash assuming that sequence-region is present in a properly formatted GFF file
+		if ( $line=~ m/^##sequence-region/ ){
+			##sequence-region   DC3.0sc00 1 34736251
+			my @seq_region = split ( ' ', $line);
+			$seq_region[1] =~ s/$chrprefix//;						#remove genome version and chr, so only chr number
+			$scaffold_last_gene_id{$seq_region[1]} = $seed; 		#init counter from 990 to match with logic in update_maker_names_fasta_chrlocs.pl???
+		}
+
 		next;
 	}
 
-	my $gff_feafures = gff3_parse_feature ($line);
+	my $gff_features = gff3_parse_feature ($line);
 
-	# if first or new gene, create the OGS id
+	# if gene, create the OGS id Dcitr00g00990.1.1
+	if ( $gff_features->{'type'} eq 'gene' ){
+		my $scaffold = $gff_features->{'seq_id'};
+		$scaffold    =~ s/$chrprefix//;						#remove genome version and chr, so only chr number
+
+		my $gene_id;
+		if ( $scaffold_last_gene_id{$scaffold} == $seed ) {	# first gene on scaffold
+			$gene_id = $seed;
+		}
+		else{
+			$scaffold_last_gene_id{$scaffold} += 10;		#namespace for 9 new genes now
+			$gene_id = $scaffold_last_gene_id{$scaffold};
+		}
+
+		my $new_id;
+
+		my @chars = split //,$gene_id;
+		my $padding_count = 5 - scalar @chars;				#presuming a gene space of <1,00,000 so 6 characters, e.g. 00001 - 99999, was 1 mill earlier
+		
+		$new_id = $prefix.$scaffold . 'g';					#prefix with chr number and 0's 
+		foreach (1..$padding_count){
+			$new_id = $new_id . '0';
+		}
+		$new_id = $new_id . '.1';							#assign version 1
+
+		#add new_id to gene index
+		my $old_id = $gff_features->{'attribute'}->{'ID'};
+		$gene_old_new_index{$old_id} = $new_id;
+
+		#create the new GFF record
+		my $gene_attributes_hashref = gff3_parse_attributes ("ID=$new_id;Name=$new_id");
+		$gff_features->{'attributes'} = $gene_attributes_hashref;
+
+		$gff_output = $gff_output . gff3_format_feature ($gff_features);
+	}
 
 
-	# if mRNA
-
-	# increment the mRNA isoform count if this is the not the first mRNA for this gene
-
+	# if mRNA, increment the mRNA isoform count if this is the not the first mRNA for this gene
+	
 
 	# get the AHRD func description, get the Apollo description if it exists
 
@@ -113,7 +154,8 @@ foreach my $line (@lines){
 
 	# write the new mRNA record 
 
-	# if any other record, use the mRNA parent to create the gff record
+	# if any other record (exon, UTRs, CDS)
+	# use the mRNA parent to create the gff record, can have multiple mRNA parents
 
 
 
@@ -163,7 +205,7 @@ sub help {
 	  -f  Curated descriptions from Apollo, e.g. 18712462-6e8c-478a-83c8-a40c9d0be977  Dihydrolipoyl transacetylase (required)
 	  -p  Prefix for name, e.g DcitrP (required)
 	  -c  Prefix for chromosome in GFF e.g. Dc3.0sc (required)
-	  -s  Starting seed, e.g. 1 (required)
+	  -s  Starting seed for gene number for each chromosome, e.g. 1000 (required)
 	  -o  output GFF file with OGS ids
 	  -h  Help
 
